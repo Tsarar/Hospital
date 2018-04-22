b5c85b8d0779a518445e21d0ad8a4c1d7ec060dd
DemonScarlett <katrinkanedavnyaya.18.10@gmail.com>
2018-04-22 16:28:06 +0300

    Test

--- HospitalInner/Hospital/Form1.cs
+++ HospitalInner/Hospital/Form1.cs
@@ -16,5 +16,10 @@ namespace Hospital
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dima durak!", "I zhopa");
+        }
     }
 }
